6168459e3b8f39216512cf956463901561ef8a14
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-12-31 12:35:08 +0300

    Added project

---
+++ DesignPatterns_Interpreter/DesignPatterns_Interpreter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DesignPatterns_Interpreter
+{
+    class Program
+    {
+        //original source https://metanit.com/sharp/patterns/3.8.php
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Design Patterns - Interpreter!");
+
+        }
+    }
+}
